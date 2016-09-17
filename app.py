701c31da1bd94161df05eec782873f2af786e5a5
jmreesjo <jmreesjo@ncsu.edu>
2016-09-17 14:07:42 -0400

    Add Firebase base

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return app.send_static_file("index.html")
