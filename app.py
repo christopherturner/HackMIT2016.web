21ecb9291171310bb5306f876776b22f9cade3ca
jmreesjo <jmreesjo@ncsu.edu>
2016-09-17 20:04:32 -0400

    Separate homepage and dashboard, implement authentication

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
+
+@app.route("/dashboard/")
+def dashboard():
+    return app.send_static_file("dashboard.html")
